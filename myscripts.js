154fa77b8bccc22d53b486afd1fcb3e263523df0
Demark <demarcuslangston@gmail.com>
2025-09-05 23:52:08 -0400

    Create HTML and Javascript Files

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+console.log("Hello World!")
+
+function getComputerChoice {
+    choices = ['Rock', 'Paper', 'Scissors']
+    choice = Math.floor(Math.random() * choices.length);
+    return choice
+}
